922e92c9c9bbb5ee3b3945246066ba29c708238b
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2021-01-29 15:32:43 +0100

    Add emoji easter egg

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,25 +8,31 @@ import Footer from "@components/Footer";
 
 const App = () => {
   return (
-    <SiteWrapper>
-      <CSSReset />
-      <GlobalStyles />
-      <Header>Get Your Shit Done</Header>
-      <Content>
-        <TasksWrapper />
-      </Content>
-      <Footer>
-        If you insist, feel free to look at the{" "}
-        <a
-          href="https://github.com/jakobsen/react-todo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          source code
-        </a>{" "}
-        for this 💩.
-      </Footer>
-    </SiteWrapper>
+    <React.StrictMode>
+      <SiteWrapper>
+        <CSSReset />
+        <GlobalStyles />
+        <Header>Get Your Shit Done</Header>
+        <Content>
+          <TasksWrapper />
+        </Content>
+        <Footer>
+          If you insist, feel free to look at the{" "}
+          <a
+            href="https://github.com/jakobsen/react-todo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            source code
+          </a>{" "}
+          for this{" "}
+          <RotateOnHover role="img" aria-label="shit">
+            💩
+          </RotateOnHover>
+          .
+        </Footer>
+      </SiteWrapper>
+    </React.StrictMode>
   );
 };
 
@@ -41,6 +47,14 @@ const SiteWrapper = styled.div`
 
 const Content = styled.main`
   flex: 1 0 auto;
+`;
+
+const RotateOnHover = styled.span`
+  display: inline-block;
+  transition: all 0.4s ease-in-out;
+  &:hover {
+    transform: rotate(360deg);
+  }
 `;
 
 export default App;
